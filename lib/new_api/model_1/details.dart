a062c5920a6fc8b37f6e5ee6a56d49b29cdb2b78
rontu mohonto <newscenery28@gmail.com>
2026-01-11 01:21:02 +0600

    tes44

--- lib/new_api/model_1/details.dart
+++ lib/new_api/model_1/details.dart
@@ -1,3 +1,23 @@
-class postdetailsModel {
+import 'dart:convert';
 
+import 'dart:convert';
+
+//the model of calling api
+class postdetailsModel {
+  final int id;
+  final String quote;
+  final String author;
+  postdetailsModel({
+    required this.id,
+    required this.quote,
+    required this.author,
+  });
+
+  factory postdetailsModel.fromjson(Map<String, dynamic> json) {
+    return postdetailsModel(
+      id: json['id'],
+      quote: json['quote'],
+      author: json['author'],
+    );
+  }
 }
